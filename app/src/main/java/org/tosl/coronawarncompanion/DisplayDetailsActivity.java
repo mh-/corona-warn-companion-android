4afe14228af6ab2846aa84d0539dd2f4f6c3014b
Michael Huebler <michael@huebler.org>
2020-08-01 00:22:26 +0200

    Added Bar Chart on Details View. It's not yet responsive.

--- app/src/main/java/org/tosl/coronawarncompanion/DisplayDetailsActivity.java
+++ app/src/main/java/org/tosl/coronawarncompanion/DisplayDetailsActivity.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ListView;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.ValueFormatter;
 
 import org.tosl.coronawarncompanion.diagnosiskeys.DiagnosisKeysProtos;
 import org.tosl.coronawarncompanion.gmsreadout.ContactRecordsProtos;
@@ -31,6 +37,10 @@ public class DisplayDetailsActivity extends AppCompatActivity {
     private static final String TAG = "DisplayDetailsActivity";
     private static boolean DEMO_MODE;
     CWCApplication app = null;
+
+    private BarChart chart;
+    private final int gridColor = Color.parseColor("#E0E0E0");
+    private final int matchBarColor = Color.parseColor("#FF0000");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,8 +73,6 @@ public class DisplayDetailsActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.textView);
         textView.setText("Matches on "+dateStr);
 
-        // ListView listView = findViewById(R.id.listView);
-
         LinkedList<Matcher.MatchEntry> matches = app.getMatches();
         if (matches != null) {
             int[] numMatchesPerHour = new int[24];
@@ -78,22 +86,72 @@ public class DisplayDetailsActivity extends AppCompatActivity {
                 // UTC because we don't want Calendar to do additional time zone compensation
                 startTime.setTimeInMillis(getMillisFromSeconds(match.startTimestampLocalTZ));
                 int startHour = startTime.get(Calendar.HOUR_OF_DAY);
-                Log.d(TAG, "Hour: "+startHour);
-
-                Log.d(TAG, "Number of Scans: "+contactRecords.getRecordCount());
-
+                //Log.d(TAG, "Hour: "+startHour);
+                numMatchesPerHour[startHour]++;
             }
 
-            /*
+            chart = findViewById(R.id.chart);
             List<BarEntry> dataPoints1 = new ArrayList<>();
-            int count = 0;
-            for (Integer daysSinceEpochLocalTZ : rpiListDaysSinceEpochLocalTZ) {
-                int numEntries = rpiList.getRpiCountForDaysSinceEpochLocalTZ(daysSinceEpochLocalTZ);
-                //Log.d(TAG, "Datapoint: " + daysSinceEpochLocalTZ + ": " + numEntries);
-                dataPoints1.add(new BarEntry(daysSinceEpochLocalTZ, numEntries));
-                count += numEntries;
+            for (int i=0; i<=23; i++) {
+                dataPoints1.add(new BarEntry(i, numMatchesPerHour[i]));
             }
-            */
+
+            BarDataSet dataSet1 = new BarDataSet(dataPoints1, "Matches"); // add entries to dataSet1
+            dataSet1.setAxisDependency(YAxis.AxisDependency.LEFT);
+            dataSet1.setColor(matchBarColor);
+
+            BarData barData1 = new BarData(dataSet1);
+            dataSet1.setHighlightEnabled(true);
+            chart.setData(barData1);
+
+            // the labels that should be drawn on the XAxis
+            ValueFormatter xAxisFormatter1 = new ValueFormatter() {
+                @Override
+                public String getAxisLabel(float value, AxisBase axis) {
+                    return String.valueOf((int) value);
+                }
+            };
+            // the labels that should be drawn on the YAxis
+            ValueFormatter yAxisFormatter1 = new ValueFormatter() {
+                @Override
+                public String getAxisLabel(float value, AxisBase axis) {
+                    return String.format("%5d", (int) value);
+                }
+            };
+            // the bar labels
+            ValueFormatter BarFormatter1 = new ValueFormatter() {
+                @Override
+                public String getBarLabel(BarEntry barEntry) {
+                    return String.valueOf((int) barEntry.getY());
+                }
+            };
+            barData1.setValueFormatter(BarFormatter1);
+            XAxis xAxis = chart.getXAxis();
+            xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+            xAxis.setValueFormatter(xAxisFormatter1);
+            xAxis.setGranularity(1.0f); // minimum axis-step (interval) is 1
+            xAxis.setGranularityEnabled(true);
+            xAxis.setDrawGridLines(false);
+
+            YAxis yAxis = chart.getAxisLeft();
+            yAxis.setGranularity(1.0f); // minimum axis-step (interval) is 1
+            yAxis.setGranularityEnabled(true);
+            yAxis.setAxisMinimum(0.0f);
+            yAxis.setGridColor(gridColor);
+            yAxis.setValueFormatter(yAxisFormatter1);
+
+            chart.getAxisRight().setAxisMinimum(0.0f);
+            chart.getAxisRight().setDrawLabels(false);
+            chart.getLegend().setEnabled(false);
+            chart.getDescription().setEnabled(false);
+            chart.setScaleYEnabled(false);
+            chart.setScaleXEnabled(true);
+            chart.invalidate(); // refresh
+
+
+            // ListView listView = findViewById(R.id.listView);
+
+
         }
     }
 }
