e5c83dce4f2bc69789dced44a3e20ee49d8a5e31
Urs Hengartner <uhengart@users.noreply.github.com>
2020-11-02 21:12:33 -0500

    Make sure that a valid map is actually shown

--- corona-warn-companion/src/main/java/org/tosl/coronawarncompanion/matchentries/MatchesRecyclerViewAdapter.java
+++ corona-warn-companion/src/main/java/org/tosl/coronawarncompanion/matchentries/MatchesRecyclerViewAdapter.java
@@ -268,6 +268,7 @@ public class MatchesRecyclerViewAdapter extends RecyclerView.Adapter<MatchesRecy
             marker.setPosition(point);
             marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
             marker.setTitle("Point of exposure");
+            holder.mMapView.setVisibility(View.VISIBLE);
             holder.mMapView.setExpectedCenter(point);
             holder.mMapView.getOverlays().add(marker);
             holder.mMapView.invalidate();
